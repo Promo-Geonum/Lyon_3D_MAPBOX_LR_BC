acc67d2c22d8a8f463e5bc5026ae1942315a82d4
Leopold-geo <56163025+Leopold-geo@users.noreply.github.com>
2020-12-16 15:02:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoibHJvYml0YWlsbGUiLCJhIjoiY2tpaGh3ZTIyMHAzdDJ3bXFkczVhOWd6ZyJ9.rQT1IDn-ZJyBPXiM49CsDA';
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+        center: [4.83789, 45.759], // starting position [lng, lat]
+        zoom: 10 // starting zoom
+    });
+
+    var url = 'https://download.data.grandlyon.com/wfs/grandlyon?SERVICE=WFS&VERSION=2.0.0&request=GetFeature&typename=fpc_fond_plan_communaut.fpctoit&outputFormat=application/json; subtype=geojson&SRSNAME=EPSG:4326&startIndex=0';
+    
+    map.on('load', function () {
+        map.addSource('toit_plat_lyon', {type: 'geojson', data: url}),
+        map.addLayer({
+            'id': 'toit_plat_lyon_3D',
+            'type': 'fill-extrusion',
+            'source': 'toit_plat_lyon',
+            'layout': {},
+            'paint': {
+                'fill-extrusion-color': '#1E90FF',
+                'fill-extrusion-opacity': 0.8,
+                'fill-extrusion-height': {'type': 'identity','property': 'htotale'},
+            }
+        });
+    });
